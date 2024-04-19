894cc468384e0483f0d7c6255926b1c50ca32a35
lsitec-piva <lucas.piva@lsitec.org.br>
2024-04-19 16:57:56 -0300

    feat: remove produto

--- app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
+++ app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
@@ -7,6 +7,7 @@ import br.com.alura.estoque.database.dao.ProdutoDAO;
 import br.com.alura.estoque.model.Produto;
 import br.com.alura.estoque.retrofit.EstoqueRetrofit;
 import br.com.alura.estoque.retrofit.callback.BaseCallBack;
+import br.com.alura.estoque.retrofit.callback.VoidCallBack;
 import br.com.alura.estoque.retrofit.service.ProdutoService;
 import retrofit2.Call;
 
@@ -104,6 +105,33 @@ public class ProdutoRepository {
         new BaseAsyncTask<>(() -> {
             dao.atualiza(produto);
             return produto;
+        }, callback::quandoSucesso)
+                .execute();
+    }
+
+    public void remove(Produto produto, DadosCarregadosCallback<Void> callback) {
+        removeNaApi(produto, callback);
+    }
+
+    private void removeNaApi(Produto produto, DadosCarregadosCallback<Void> callback) {
+        Call<Void> call = service.remove(produto.getId());
+        call.enqueue(new VoidCallBack(new VoidCallBack.RespostaCallback() {
+            @Override
+            public void quandoSucesso() {
+                removeInterno(produto, callback);
+            }
+
+            @Override
+            public void quandoFalha(String erro) {
+                callback.quandoFalha(erro);
+            }
+        }));
+    }
+
+    private void removeInterno(Produto produto, DadosCarregadosCallback<Void> callback) {
+        new BaseAsyncTask<>(() -> {
+            dao.remove(produto);
+            return null;
         }, callback::quandoSucesso)
                 .execute();
     }
--- app/src/main/java/br/com/alura/estoque/retrofit/service/ProdutoService.java
+++ app/src/main/java/br/com/alura/estoque/retrofit/service/ProdutoService.java
@@ -5,13 +5,13 @@ import java.util.List;
 import br.com.alura.estoque.model.Produto;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ProdutoService {
-
     @GET("produto")
     Call<List<Produto>> buscaTodos();
 
@@ -20,4 +20,7 @@ public interface ProdutoService {
 
     @PUT("produto/{id}")
     Call<Produto> edita(@Path("id") long id, @Body Produto produto);
+
+    @DELETE("produto/{id}")
+    Call<Void> remove(@Path("id") long id);
 }
